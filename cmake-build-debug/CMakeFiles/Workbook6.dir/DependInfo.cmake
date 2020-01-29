3ed9da5a74dfedd98498ec7f17d03c29be61dbd9
John Dickson <jhdickson@outlook.com>
2020-01-29 23:19:00 +0000

    more fiddling with malloc

---
+++ cmake-build-debug/CMakeFiles/Workbook6.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/jhdickson/Git/Workbook6/csv-vsc.c" "/home/jhdickson/Git/Workbook6/cmake-build-debug/CMakeFiles/Workbook6.dir/csv-vsc.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Workbook6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Workbook6.dir/csv-vsc.c.o"
+  "Workbook6"
+  "Workbook6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Workbook6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
